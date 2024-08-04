0981a830314e098a9cac3d6066c49da7e315286e
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-08-04 23:53:51 +0000

    install remix and shadcn ui

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+import * as tailwindcss_animate from "tailwindcss-animate";
+
+export default {
+  darkMode: ["class"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [tailwindcss_animate],
+} satisfies Config;
